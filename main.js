6c4470db80b9720c558b4752169daba8208ae574
facucarrion <facundojcarrion@gmail.com>
2022-07-16 16:54:56 -0300

    fixing: responsive design

---
+++ main.js
@@ -0,0 +1,31 @@
+// Mailto
+
+const $form = document.querySelector('#contactform')
+const $buttonMailto = document.querySelector('#mailto')
+
+$form.addEventListener('submit', handleSubmit)
+
+function handleSubmit(event) {
+  event.preventDefault()
+  const form = new FormData(this)
+  $buttonMailto.setAttribute('href', `mailto:focsolar@gmail.com?subject=${form.get('name')}&body=${form.get('message')}`)
+  $buttonMailto.click()
+}
+
+// Menu Responsive
+
+const $btnOpen = document.getElementById('open');
+const $btnClose = document.getElementById('close');
+const $menu = document.querySelector('.header__nav--menu');
+
+$btnOpen.addEventListener('click', () => {
+    $btnOpen.style.display = 'none';
+    $btnClose.style.display = 'block';
+    $menu.style.transform = 'translateX(0)';
+})
+
+$btnClose.addEventListener('click', () => {
+  $btnOpen.style.display = 'block';
+  $btnClose.style.display = 'none';
+  $menu.style.transform = 'translateX(300px)';
+})
