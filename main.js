69021c7be35eb87597792f70b8b55783bb19e033
facucarrion <facundojcarrion@gmail.com>
2022-07-18 11:44:51 -0300

    Edited: smooth scroll time, nav article transition

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ $(document).ready(function () {
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('*').animate({
         scrollTop: $(hash).offset().top - hashWidth
-      }, 500, function () {
+      }, 700, function () {
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         // window.location.hash = hash;
